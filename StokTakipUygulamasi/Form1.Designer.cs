edf6de9f14999738531bd48f3f23a359250660eb
MucahitErkek <94393687+mucahiderkek@users.noreply.github.com>
2025-02-19 10:05:58 +0300

    Stok-Takip-Uygulaması
    Basit Bir Stok Takip Uygulaması

---
+++ StokTakipUygulamasi/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace StokTakipUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvUrunler = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUrunAdi = new System.Windows.Forms.TextBox();
+            this.txtStokMiktari = new System.Windows.Forms.TextBox();
+            this.txtFiyat = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAra = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUrunler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvUrunler
+            // 
+            this.dgvUrunler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUrunler.Location = new System.Drawing.Point(12, 201);
+            this.dgvUrunler.Name = "dgvUrunler";
+            this.dgvUrunler.RowHeadersWidth = 51;
+            this.dgvUrunler.RowTemplate.Height = 24;
+            this.dgvUrunler.Size = new System.Drawing.Size(1065, 269);
+            this.dgvUrunler.TabIndex = 0;
+            this.dgvUrunler.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvUrunler_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ürün Adı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Stok Miktarı:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fiyat";
+            // 
+            // txtUrunAdi
+            // 
+            this.txtUrunAdi.Location = new System.Drawing.Point(152, 24);
+            this.txtUrunAdi.Name = "txtUrunAdi";
+            this.txtUrunAdi.Size = new System.Drawing.Size(100, 22);
+            this.txtUrunAdi.TabIndex = 4;
+            // 
+            // txtStokMiktari
+            // 
+            this.txtStokMiktari.Location = new System.Drawing.Point(152, 54);
+            this.txtStokMiktari.Name = "txtStokMiktari";
+            this.txtStokMiktari.Size = new System.Drawing.Size(100, 22);
+            this.txtStokMiktari.TabIndex = 5;
+            // 
+            // txtFiyat
+            // 
+            this.txtFiyat.Location = new System.Drawing.Point(152, 81);
+            this.txtFiyat.Name = "txtFiyat";
+            this.txtFiyat.Size = new System.Drawing.Size(100, 22);
+            this.txtFiyat.TabIndex = 6;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(370, 17);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 7;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(370, 59);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 8;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(370, 98);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 9;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(541, 24);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(75, 23);
+            this.btnAra.TabIndex = 10;
+            this.btnAra.Text = "Ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAra
+            // 
+            this.txtAra.Location = new System.Drawing.Point(632, 24);
+            this.txtAra.Name = "txtAra";
+            this.txtAra.Size = new System.Drawing.Size(100, 22);
+            this.txtAra.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 482);
+            this.Controls.Add(this.txtAra);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtFiyat);
+            this.Controls.Add(this.txtStokMiktari);
+            this.Controls.Add(this.txtUrunAdi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvUrunler);
+            this.Name = "Form1";
+            this.Text = "Stok Takip Sistemi";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUrunler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvUrunler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUrunAdi;
+        private System.Windows.Forms.TextBox txtStokMiktari;
+        private System.Windows.Forms.TextBox txtFiyat;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAra;
+    }
+}
+
---
+++ StokTakipUygulamasi/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+
+namespace StokTakipUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        string dbPath = "stoktakip.sqlite"; // Veritabanı dosyası
+        string connectionString;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = $"Data Source={dbPath};Version=3;";
+            VeritabaniOlustur(); // Uygulama açılınca kontrol et
+            UrunleriListele();
+        }
+
+        private void VeritabaniOlustur()
+        {
+            if (!File.Exists(dbPath)) // Eğer veritabanı dosyası yoksa oluştur
+            {
+                SQLiteConnection.CreateFile(dbPath);
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = @"CREATE TABLE Urunler (
+                        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                        UrunAdi TEXT NOT NULL,
+                        StokMiktari INTEGER NOT NULL,
+                        Fiyat REAL NOT NULL
+                    );";
+
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                }
+                MessageBox.Show("Veritabanı oluşturuldu!");
+            }
+        }
+        private void KategoriTablosuOlustur()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "CREATE TABLE IF NOT EXISTS Kategoriler (ID INTEGER PRIMARY KEY AUTOINCREMENT, KategoriAdi TEXT NOT NULL)";
+
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
+        }
+
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "INSERT INTO Urunler (UrunAdi, StokMiktari, Fiyat) VALUES (@urunAdi, @stokMiktari, @fiyat)";
+
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("@urunAdi", txtUrunAdi.Text);
+                    cmd.Parameters.AddWithValue("@stokMiktari", Convert.ToInt32(txtStokMiktari.Text));
+                    cmd.Parameters.AddWithValue("@fiyat", Convert.ToDouble(txtFiyat.Text));
+
+                    cmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+                MessageBox.Show("Ürün başarıyla eklendi!");
+                UrunleriListele(); // Güncellenmiş listeyi getir
+            }
+            txtUrunAdi.Clear();
+            txtStokMiktari.Clear();
+            txtFiyat.Clear();
+        }
+        private void UrunleriListele()
+        {
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Urunler";
+                using (var cmd = new SQLiteCommand(sql, conn))
+                {
+                    using (var da = new SQLiteDataAdapter(cmd))
+                    {
+                        System.Data.DataTable dt = new System.Data.DataTable();
+                        da.Fill(dt);
+                        dgvUrunler.DataSource = dt; // DataGridView'e verileri yükle
+                    }
+                }
+                conn.Close();
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (dgvUrunler.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvUrunler.SelectedRows[0].Cells["ID"].Value);
+
+                using (var conn = new SQLiteConnection(connectionString))
+                {
+                    conn.Open();
+                    string sql = "UPDATE Urunler SET UrunAdi=@urunAdi, StokMiktari=@stokMiktari, Fiyat=@fiyat WHERE ID=@id";
+
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@urunAdi", txtUrunAdi.Text);
+                        cmd.Parameters.AddWithValue("@stokMiktari", Convert.ToInt32(txtStokMiktari.Text));
+                        cmd.Parameters.AddWithValue("@fiyat", Convert.ToDouble(txtFiyat.Text));
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    conn.Close();
+                    MessageBox.Show("Ürün başarıyla güncellendi!");
+                    UrunleriListele(); // Listeyi yenile
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen güncellenecek bir ürünü seçin.");
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            if (dgvUrunler.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvUrunler.SelectedRows[0].Cells["ID"].Value);
+
+                DialogResult result = MessageBox.Show("Bu ürünü silmek istediğinizden emin misiniz?", "Silme Onayı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    using (var conn = new SQLiteConnection(connectionString))
+                    {
+                        conn.Open();
+                        string sql = "DELETE FROM Urunler WHERE ID=@id";
+
+                        using (var cmd = new SQLiteCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@id", id);
+                            cmd.ExecuteNonQuery();
+                        }
+
+                        conn.Close();
+                        MessageBox.Show("Ürün başarıyla silindi!");
+                        UrunleriListele(); // Listeyi yenile
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Lütfen silinecek bir ürünü seçin.");
+            }
+        }
+
+        private void dgvUrunler_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) // Tıklanan satır geçerli mi?
+            {
+                txtUrunAdi.Text = dgvUrunler.Rows[e.RowIndex].Cells["UrunAdi"].Value.ToString();
+                txtStokMiktari.Text = dgvUrunler.Rows[e.RowIndex].Cells["StokMiktari"].Value.ToString();
+                txtFiyat.Text = dgvUrunler.Rows[e.RowIndex].Cells["Fiyat"].Value.ToString();
+            }
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            string aramaKelimesi = txtAra.Text.Trim(); // Kullanıcının yazdığı metni al
+
+            // Eğer kullanıcı hiçbir şey yazmadıysa, tüm ürünleri listele
+            if (string.IsNullOrWhiteSpace(aramaKelimesi))
+            {
+                UrunleriListele(); // Tüm ürünleri göster
+                return;
+            }
+
+            // Arama işlemini yap
+            using (var conn = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM Urunler WHERE UrunAdi LIKE @urunAdi"; // Arama kriterini belirledik
+                    using (var cmd = new SQLiteCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@urunAdi", "%" + aramaKelimesi + "%"); // % ile eşleşme sağlıyoruz
+
+                        using (var da = new SQLiteDataAdapter(cmd))
+                        {
+                            var dt = new System.Data.DataTable();
+                            da.Fill(dt);
+                            dgvUrunler.DataSource = dt; // DataGridView'e sonuçları yüklüyoruz
+                        }
+                    }
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata: " + ex.Message); // Hata mesajı göster
+                }
+            }
+        }
+
+
+
+    }
+}
